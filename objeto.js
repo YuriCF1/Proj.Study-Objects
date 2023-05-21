8a2d10d7011cc03a2a775cce347d05af46e8c983
Yuri <yurifdev@gmail.com>
2023-05-21 15:21:54 -0300

    Starting project

---
+++ objeto.js
@@ -0,0 +1,9 @@
+const listaCPFs = [111, 222, 333];
+const informacoesPessoa = ["nome", "Jose", "idade", 32, "CPF"];
+console.log(informacoesPessoa[1]);
+
+const objetoPessoa = {
+  nome: "Jose",
+  idade: 32,
+  cpf: 111,
+};
