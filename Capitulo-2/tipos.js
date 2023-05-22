929ac7a89d5578981fcc51e8f901a3ae30eddce1
Yuri <yurifdev@gmail.com>
2023-05-22 19:10:40 -0300

    Adding a  list to the object

---
+++ Capitulo-2/tipos.js
@@ -0,0 +1,10 @@
+const cliente = {
+  nome: "Ze",
+  idade: 24,
+  email: "joao@top.com",
+  telefone: ["115555555", "114444444"]
+//   telefon2: "114444444",
+};
+
+console.log(cliente);
+console.log(cliente.telefone);
