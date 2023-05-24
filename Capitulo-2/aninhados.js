a67f2e669ee415b7ecb260f9371c3cc6f47a1301
Yuri <yurifdev@gmail.com>
2023-05-24 16:29:56 -0300

    Adding object insde another object

---
+++ Capitulo-2/aninhados.js
@@ -0,0 +1,15 @@
+const cliente = {
+  nome: "Don Pedro II",
+  idade: 32,
+  email: "pindorama@reino.com",
+  telefone: ["114444444", "115555555"],
+};
+
+cliente.endereco = {
+  rua: "R. Isabel",
+  numer: 1808,
+  apartamento: true,
+  complemento: "ap 07",
+};
+
+console.log(cliente.endereco);
