450eabe44075cb407d14cbc2bf878fce54b645d3
Yuri <yurifdev@gmail.com>
2023-05-28 18:04:00 -0300

    Using stringify and parse

--- Capitulo-4/leitura.js
+++ Capitulo-4/leitura.js
@@ -1,3 +1,12 @@
 const dados = require("./cliente.json");
-console.log(dados);
-console.log(typeof dados);
+// console.log(dados);
+// console.log(typeof dados);
+
+const clienteEmString = JSON.stringify(dados)
+
+console.log(clienteEmString);
+console.log(typeof clienteEmString);
+
+const clienteEmObjeto = JSON.parse(clienteEmString)
+
+console.log(clienteEmObjeto);
